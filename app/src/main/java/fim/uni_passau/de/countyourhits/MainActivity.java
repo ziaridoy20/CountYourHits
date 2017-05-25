85032a09a22671fda820c269b131beafbd7f31b9
Ziauddin Ridoy <ziaridoy20@gmail.com>
2017-05-25 17:14:52 +0200

    Initial commit

---
+++ app/src/main/java/fim/uni_passau/de/countyourhits/MainActivity.java
@@ -0,0 +1,28 @@
+package fim.uni_passau.de.countyourhits;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    static {
+        if (!OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV not loaded");
+        }else{
+            Log.d(TAG,"OpenCV loaded");
+        }
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
