4d3966477c7fb4b4b3f9c23c42655f7ceea5a62f
subash <subashale@gamil.com>
2017-06-11 17:49:53 +0200

    updated main activity

--- app/src/main/java/fim/uni_passau/de/countyourhits/activity/MainActivity.java
+++ app/src/main/java/fim/uni_passau/de/countyourhits/activity/MainActivity.java
@@ -1,105 +1,27 @@
 package fim.uni_passau.de.countyourhits.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.SurfaceView;
-
 import android.view.View;
-import android.view.WindowManager;
-
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Mat;
 
 import fim.uni_passau.de.countyourhits.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener,CameraBridgeViewBase.CvCameraViewListener2 {
-
-    private CameraBridgeViewBase mOpenCvCameraView;
-    private Mat mRgba;
-
-    private BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status){
-                case LoaderCallbackInterface.SUCCESS:{
-                    mOpenCvCameraView.enableView();
-                    mOpenCvCameraView.setOnTouchListener(MainActivity.this);
-                }
-                break;
-                default: {
-                    super.onManagerConnected(status);
-                }
-                break;
-            }
-
-
-        }
-    };
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
-
-        mOpenCvCameraView = (CameraBridgeViewBase)findViewById(R.id.opencv_activity_surface_view);
-        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-        mOpenCvCameraView.setCvCameraViewListener(this);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if(mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-
+    public void btnCameraClick(View view) {
+        Intent cameraIntent = new Intent(this, ColorBlobDetectionActivity.class);
+        startActivity(cameraIntent);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if(!OpenCVLoader.initDebug()){
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0,this, mLoaderCallBack);
-        }else{
-            mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if(mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-
-    }
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-        mRgba = new Mat();
-
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-        mRgba.release();
-
-    }
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        mRgba = inputFrame.rgba();
-        return mRgba;
-    }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        return false;
+    public void btnConnectionClick(View view) {
+        Intent connectionIntent = new Intent(this, DartWiFiDirectActivity.class);
+        startActivity(connectionIntent);
     }
 }
