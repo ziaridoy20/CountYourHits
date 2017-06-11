2b668a9c5a27fcd1e3bdadbb9f96b1d2e363cbda
subash <subashale@gamil.com>
2017-06-11 16:58:15 +0200

    integrate connection activity

--- openCVLibrary320/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary320/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: H:\\University of Passau\\Projects\\DirtHit\\openCVLibrary320\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\subash\\Downloads\\DartHits\\openCVLibrary320\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
