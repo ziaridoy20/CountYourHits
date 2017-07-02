b1d24cc01144120d55e73a239f2a48791312c289
subash <subashale@gamil.com>
2017-07-02 17:41:02 +0200

    db

--- app/src/main/java/fim/uni_passau/de/countyourhits/app/Helper.java
+++ app/src/main/java/fim/uni_passau/de/countyourhits/app/Helper.java
@@ -2,11 +2,17 @@ package fim.uni_passau.de.countyourhits.app;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.icu.text.SimpleDateFormat;
 import android.os.Environment;
+import android.text.format.DateFormat;
 import android.util.Base64;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Date;
 import java.text.DecimalFormat;
 
 /**
@@ -15,35 +21,36 @@ import java.text.DecimalFormat;
 
 public class Helper {
 
-    public static String bitmapToString(Bitmap bitmap){
-        ByteArrayOutputStream baos=new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
-        byte [] arr=baos.toByteArray();
-        String result= Base64.encodeToString(arr, Base64.DEFAULT);
+    public static String bitmapToString(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] arr = baos.toByteArray();
+        String result = Base64.encodeToString(arr, Base64.DEFAULT);
         return result;
     }
 
-    public static Bitmap stringToBitmap(String image){
-        try{
-            byte [] encodeByte=Base64.decode(image,Base64.DEFAULT);
-            Bitmap bitmap= BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+    public static Bitmap stringToBitmap(String image) {
+        try {
+            byte[] encodeByte = Base64.decode(image, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
             return bitmap;
-        }catch(Exception e){
+        } catch (Exception e) {
             e.getMessage();
             return null;
         }
     }
 
-    public static File getRootDirectory(){
+    public static File getRootDirectory() {
         return Environment.getExternalStorageDirectory();
     }
 
-    public static String getRootDirectoryPath(){
+    public static String getRootDirectoryPath() {
         return Environment.getExternalStorageDirectory().toString();
     }
 
     public static String convertDouble2String(double mDbl) {
         DecimalFormat df = new DecimalFormat("#.###");
         return df.format(mDbl);
+
     }
 }
