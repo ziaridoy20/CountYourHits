70ad7c8df5e68fff7567145ecf0139f3f22fa634
subash <subashale@gamil.com>
2017-07-02 19:46:37 +0200

    Added Message class

---
+++ app/src/main/java/fim/uni_passau/de/countyourhits/model/Message.java
@@ -0,0 +1,31 @@
+package fim.uni_passau.de.countyourhits.model;
+
+/**
+ * Created by Nahid 002345 on 6/12/2017.
+ */
+
+import com.bluelinelabs.logansquare.annotation.JsonField;
+import com.bluelinelabs.logansquare.annotation.JsonObject;
+
+/**
+ * Created by Nahid 002345 on 6/11/2017.
+ */
+
+@JsonObject
+public class Message{
+
+    /*
+     * Annotate a field that you want sent with the @JsonField marker.
+     */
+    @JsonField
+    public String description;
+
+    @JsonField
+    public String imgBlob;
+    /*
+     * Note that since this field isn't annotated as a
+     * @JsonField, LoganSquare will ignore it when parsing
+     * and serializing this class.
+     */
+    public int nonJsonField;
+}
