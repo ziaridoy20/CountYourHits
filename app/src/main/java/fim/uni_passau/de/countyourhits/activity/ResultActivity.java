d8458cfb36eadf35e08db0869e5d6765c1ad7ce7
Roji Maharjan <loceehide@gmail.com>
2017-07-08 23:13:48 +0200

    result activity changed

--- app/src/main/java/fim/uni_passau/de/countyourhits/activity/ResultActivity.java
+++ app/src/main/java/fim/uni_passau/de/countyourhits/activity/ResultActivity.java
@@ -1,14 +1,28 @@
 package fim.uni_passau.de.countyourhits.activity;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.peak.salut.Callbacks.SalutCallback;
+import com.peak.salut.Callbacks.SalutDataCallback;
+import com.peak.salut.Callbacks.SalutDeviceCallback;
+import com.peak.salut.Salut;
+import com.peak.salut.SalutDataReceiver;
+import com.peak.salut.SalutDevice;
+import com.peak.salut.SalutServiceData;
 import com.yarolegovich.discretescrollview.DiscreteScrollView;
 import com.yarolegovich.discretescrollview.InfiniteScrollAdapter;
 import com.yarolegovich.discretescrollview.Orientation;
@@ -22,7 +36,7 @@ import fim.uni_passau.de.countyourhits.adapter.ResultAdapter;
 import fim.uni_passau.de.countyourhits.model.ResultResponse;
 
 public class ResultActivity extends AppCompatActivity implements DiscreteScrollView.OnItemChangedListener,
-        View.OnClickListener  {
+        View.OnClickListener ,SalutDataCallback {
     private List<ResultResponse> data;
 
     private TextView currentItemName;
@@ -30,11 +44,24 @@ public class ResultActivity extends AppCompatActivity implements DiscreteScrollV
     private ImageView rateItemButton;
     private DiscreteScrollView itemPicker;
     private InfiniteScrollAdapter infiniteAdapter;
+    //
+    //salut p2p connection
+    //
+    public static final String TAG = "Salut";
+    public SalutDataReceiver dataReceiver;
+    public SalutServiceData serviceData;
+    public Salut network;
+    SalutDataCallback callback;
+    ProgressBar progressBar;
+    CardView image;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+
 
         currentItemName = (TextView) findViewById(R.id.item_name);
         currentItemPrice = (TextView) findViewById(R.id.item_price);
@@ -62,8 +89,21 @@ public class ResultActivity extends AppCompatActivity implements DiscreteScrollV
         //findViewById(R.id.home).setOnClickListener(this);
         //findViewById(R.id.btn_smooth_scroll).setOnClickListener(this);
         //findViewById(R.id.btn_transition_time).setOnClickListener(this);
+        initSalut();
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(network != null) {
+            if (network.isRunningAsHost) {
+                network.stopNetworkService(false);
+
+            } else if(network.isDiscovering) {
+                network.stopServiceDiscovery(false);
+            }
+        }
+    }
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -113,5 +153,106 @@ public class ResultActivity extends AppCompatActivity implements DiscreteScrollV
                 new ResultResponse("1", "Everyday Candle", "$12.00 USD"),
                 new ResultResponse("2", "Small Porcelain Bowl", "$50.00 USD"),
                 new ResultResponse("3", "Favourite Board", "$265.00 USD"));
+    }
+
+    public void initSalut(){
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+        image =(CardView) findViewById(R.id.card_view);
+
+        dataReceiver = new SalutDataReceiver(this,this);
+        serviceData = new SalutServiceData("wifiservice", 13334,"P2P");
+        network = new Salut(dataReceiver, serviceData, new SalutCallback() {
+            @Override
+            public void call() {
+                Log.e(TAG, "Sorry, but this device does not support WiFi Direct.");
+            }
+        });
+        discoverServices();
+
+    }
+    private void discoverServices() {
+        progressBar.setVisibility(View.VISIBLE);
+        image.setVisibility(View.GONE);
+        if(!network.isRunningAsHost && !network.isDiscovering) {
+            network.discoverNetworkServices(new SalutDeviceCallback() {
+                @Override
+                public void call(final SalutDevice device) {
+                    network.registerWithHost(device, new SalutCallback() {
+                        @Override
+                        public void call() {
+                            progressBar.setVisibility(View.GONE);
+                            image.setVisibility(View.VISIBLE);
+                            AlertDialog.Builder alertDialog = new AlertDialog.Builder(ResultActivity.this);
+                            alertDialog.setTitle("Client Device Connected")
+                                    .setMessage("Device: " + device.deviceName + " connected as host")
+                                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.cancel();
+                                        }
+                                    });
+                            AlertDialog alert = alertDialog.create();
+                            alertDialog.setTitle("STOP Discovery");
+                            alert.show();
+                        }
+                    }, new SalutCallback() {
+                        @Override
+                        public void call() {
+                            AlertDialog.Builder alertDialog = new AlertDialog.Builder(ResultActivity.this);
+                            alertDialog.setTitle("Unsuccessful")
+                                    .setMessage("Sorry Unable to Connect, Try again")
+                                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            progressBar.setVisibility(View.GONE);
+                                            image.setVisibility(View.VISIBLE);
+                                            dialog.cancel();
+                                        }
+                                    });
+                            AlertDialog alert = alertDialog.create();
+                            alertDialog.setTitle("STAR DISCOVERY");
+                            alert.show();
+                        }
+                    });
+                }
+            }, true);
+
+        }
+        else {
+            stopDiscovery();
+//            old code
+//            network.stopServiceDiscovery(true);
+//            discoverBtn.setText("Discover Services");
+//            hostingBtn.setAlpha(1f);
+//            hostingBtn.setClickable(false);
+        }
+    }
+
+    protected void stopDiscovery() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(ResultActivity.this);
+        alertDialog.setMessage("Do you want to STOP DISCOVERY?").setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        network.stopServiceDiscovery(true);
+                        progressBar.setVisibility(View.GONE);
+                        image.setVisibility(View.VISIBLE);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = alertDialog.create();
+        alertDialog.setTitle("Stop Discovery");
+        alert.show();
+    }
+
+    @Override
+    public void onDataReceived(Object o) {
+
     }
 }
